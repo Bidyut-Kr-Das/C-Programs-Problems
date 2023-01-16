d07f4bd7c8172d68017cede9535da22e1897f69a
Bidyut-Kr-Das <bkdas2017.bd@gmail.com>
2023-01-17 00:33:17 +0530

    error fixed when one digit is 0
    found an error when one digit is 0 - fixed

--- Nivennum.c
+++ Nivennum.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-//if sum of digit of a number can divide the number and remainder is 0 then its Niven Number
-//also called Harshad Number.
+// if sum of digit of a number can divide the number and remainder is 0 then its Niven Number
+// also called Harshad Number.
 int sum_of_digit(int num)
 {
     int sum = 0;
-    while (num % 10 != 0)
+    while (num / 10 != 0)
     {
         sum = sum + (num % 10);
         num = num / 10;
     }
+    sum = sum + num;
     return sum;
 }
 int main()
